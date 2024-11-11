36e78b2abfe73a59885d2ce3bb980a1294ad8844
Steppe Schoolkate <S.Schoolkate@hotmail.nl>
2024-11-11 16:50:01 +0100

    Added tags

--- creative.js
+++ creative.js
@@ -1,36 +1,42 @@
 var data = [
     {
         "title":"Creatures of the Deep",
-        "description": "A game for a course",
+        "description": "An atmospheric horror simulator game I made for a university course together with 5 other students. I contributed to this project by implementing the submarine control and malfunction interactions. I also created all the surface shaders for the different rusted materials.",
         "image":"images/cotd/FrontPagePic.png",
         "itch":"https://raul-martin.itch.io/creatures-of-the-deep",
-        "page":"Hello"
+        "page":"Hello",
+        "tags":["Game", "Unity"]
     },
     {
         "title":"Anomaly Clean-up Squad",
         "description": "A game for a course",
         "image":"images/acs/acs.png",
         "itch":"https://grassland.itch.io/anomaly-clean-up-sqaud",
-        "page":""
+        "page":"asdasd",
+        "tags":["Game", "Unity"]
     },
     {
         "title":"Thingamajig-inator",
         "description": "A game for a course",
         "image":"images/thingamajig/YUwX1d.gif",
         "itch":"https://buddhadom.itch.io/thingamajig-inator",
-        "page":""
+        "page":"asdasd",
+        "tags":["Game", "Unity"]
     },
     {
         "title":">Hotfix",
         "description": "A game for a course",
         "image":"images/hotfix/b0Xz57.png",
         "itch":"https://grassland.itch.io/hotfix",
-        "page":""
+        "page":"asdasd",
+        "tags":["Game", "Unity", "Game Jam"]
     }
 ]
 
 function GetCreativeWorks(id){
     
+
+
     const container = document.getElementById(id);
 
     var html = "";
@@ -41,7 +47,19 @@ function GetCreativeWorks(id){
         html += '<section>';
         html += '<a href="#" class="image"><img src="'+ obj.image + '" alt="" data-position="center center" /></a>';
         html += '<div class="content"><div class="inner">';
-        html += '<h2>' + obj.title + '</h2>';
+        
+        html += '<ul class="title-tag-list">'
+        html += '<li><h2>' + obj.title + '</h2></li>';
+        
+        for(let j = 0; j < obj.tags.length; j++) {
+            let tag = obj.tags[j];
+            html += '<li><code class="tag">' + tag + '</code></li>'
+        }
+        html += '</ul>'
+        
+        // html += '<h2>' + obj.title + '</h2>';
+
+
         html += '<p>' + obj.description + '</p>';
         html += '<ul class="actions">'
 
@@ -53,6 +71,8 @@ function GetCreativeWorks(id){
             html += '<li><a href="creative.html?work='+i+'" class="button">Read More</a></li>'
         }
 
+
+
         html += '</ul>'
         html += '</div></div>';
         html += '</section>';
@@ -62,14 +82,19 @@ container.innerHTML = html;
 }
 
 function SetCreativeWork(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const index = urlParams.get('work')
+
+
     var titles = document.getElementsByClassName('creative-title');
     for (var i = 0; i < titles.length; ++i) {
         var title = titles[i];  
-        title.innerHTML = data[0].title;
+        title.innerHTML = data[index].title;
     }
     var images = document.getElementsByClassName('creative-image');
     for (var i = 0; i < images.length; ++i) {
         var image = images[i];
-        image.setAttribute("scr", data[0].image);
+        image.setAttribute("scr", data[index].image);
     }
 }
