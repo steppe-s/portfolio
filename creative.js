a23f921e3c0c357687a1c0fe9ed11cb07c9252e9
Steppe Schoolkate <S.Schoolkate@hotmail.nl>
2024-11-11 14:57:24 +0100

    Looks better now

--- creative.js
+++ creative.js
@@ -1,9 +1,68 @@
-import data from 'data/creative_works.json' assert { type: 'json' };
+var data = [
+    {
+        "title":"Creatures of the Deep",
+        "description": "A game for a course",
+        "image":"images/FrontPagePic.png",
+        "itch":"https://raul-martin.itch.io/creatures-of-the-deep",
+        "page":"Hello"
+    },
+    {
+        "title":"Anomaly Clean-up Squad",
+        "description": "A game for a course",
+        "image":"",
+        "itch":"",
+        "page":""
+    },
+    {
+        "title":"Thingamajig-inator",
+        "description": "A game for a course",
+        "image":"YUwX1d.gif",
+        "itch":"",
+        "page":""
+    }
+]
 
-function GetCreativeWorks(){
+function GetCreativeWorks(id){
+    
+    const container = document.getElementById(id);
+
+    var html = "";
+
     for(let i = 0; i < data.length; i++) {
         let obj = data[i];
-    
-        console.log(obj.id);
+
+        html += '<section>';
+        html += '<a href="#" class="image"><img src="'+ obj.image + '" alt="" data-position="center center" /></a>';
+        html += '<div class="content"><div class="inner">';
+        html += '<h2>' + obj.title + '</h2>';
+        html += '<p>' + obj.description + '</p>';
+        html += '<ul class="actions">'
+
+        if(obj.itch){
+            html += '<li><a href="'+obj.itch+'" class="button">Itch</a></li>'
+        }
+
+        if(obj.page){
+            html += '<li><a href="creative.html?work='+i+'" class="button">Read More</a></li>'
+        }
+
+        html += '</ul>'
+        html += '</div></div>';
+        html += '</section>';
+    }
+container.innerHTML = html;
+    console.log(html);
+}
+
+function SetCreativeWork(){
+    var titles = document.getElementsByClassName('creative-title');
+    for (var i = 0; i < titles.length; ++i) {
+        var title = titles[i];  
+        title.innerHTML = data[0].title;
+    }
+    var images = document.getElementsByClassName('creative-image');
+    for (var i = 0; i < images.length; ++i) {
+        var image = images[i];
+        image.setAttribute("scr", data[0].image);
     }
 }
